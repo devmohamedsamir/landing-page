5aa6f1c2ab3250d604ff9708f108fa1e34d73541
msamir <dev.mohamed.samir@gmail.com>
2024-03-12 10:47:56 +0200

    transferred repo from my old account

---
+++ README.md
@@ -0,0 +1,27 @@
+## Project Landing Page is the  first Project for Udacity Nano degree Professional web front end 
+
+## Languages used in Landing Page Project:
+- HTML
+- CSS 
+- JavaScript
+
+## Landing Page Project Functionalities:
+- Navigation between sections.
+- The Dynamic Active when near ViewPort.
+- The Scrolling Effect.
+
+## what am i learn from this project?
+- In this project i convert static web project to a dynamic project.\
+
+##  How to use the project?
+- first download the repo.
+- second you should have a web browser like chrome or firefox or any other one 
+- third open index.html  
+- and it works now.
+![alt text](https://github.com/mosamircs/landing-page/blob/master/active-state.gif?raw=true)
+## ScreenShots
+![alt text](https://github.com/mosamircs/landing-page/blob/master/landing.png?raw=true)
+![alt text](https://github.com/mosamircs/landing-page/blob/master/section1.png?raw=true)
+![alt text](https://github.com/mosamircs/landing-page/blob/master/section2.png?raw=true)
+![alt text](https://github.com/mosamircs/landing-page/blob/master/section3.png?raw=true)
+![alt text](https://github.com/mosamircs/landing-page/blob/master/section4.png?raw=true)
---
+++ js/app.js
@@ -0,0 +1,48 @@
+
+
+/**function navBarMake work: 
+ * 
+ * this function creates the nav bar 
+ * and gives it the styles 
+ */
+function navBarMake() {
+    const sectionsList = document.querySelectorAll("section");//to get number of sections
+    var sectionCounter = 1;//start count for while loop
+    let sectionsLength = sectionsList.length;//number of sections
+    while (sectionCounter <= sectionsLength) {
+        var link = document.createElement("a");//created to be in list of navbar
+        link.href = `#section${sectionCounter}`;//the link in the navbar refer to specific section
+        link.className = "menu__link";
+        var navBarText = document.createTextNode(`Section ${sectionCounter}`);//text inside each navbar element 
+        var navBarItem = document.createElement("li");
+        navBarItem.appendChild(link);//adds each link to list element  unordered list
+        document.getElementById('navbar__list').appendChild(navBarItem);//adds linkelement  to navbar unordered list
+        link.appendChild(navBarText);
+        navBarItem.setAttribute("id", `sec${sectionCounter}`);
+        if (document.getElementById(`sec${sectionCounter}`) != "") {
+            document.getElementById(`sec${sectionCounter}`).addEventListener("click", function () {
+                this.style.backgroundColor = "black";
+                this.style.color = "white";
+                this.addEventListener("mouseout", event => {
+                    this.style.backgroundColor = "white";
+                    this.style.color = "black";
+                });
+            })
+            sectionCounter++;
+        }
+    }
+}
+
+
+//creates the nav bar
+navBarMake();
+//add class "active" to section when near to viewport
+document.addEventListener("scroll", document.querySelectorAll('section').forEach(section => {
+    //checks if the section in viewport and it d
+    if (section.getBoundingClientRect().top >= 0) {
+        if (!section.classList.contains('your-active-class')) {
+            section.classList.add("your-active-class");
+
+        }
+    }
+}));
